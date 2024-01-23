ef5b986a20202d4fe0c5179a34998c3c54ace15f
Sofiane.T <96925448+m0tyr@users.noreply.github.com>
2024-01-23 19:07:14 +0100

    ajout inscription + rectification

--- src/models/queries.js
+++ src/models/queries.js
@@ -1,3 +1,5 @@
+const { request, response } = require('express')
+
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
@@ -15,6 +17,21 @@ const getUsers = (request, response) => {
       response.status(200).json(results.rows)
     })
   }
+  const AddUser = (request, response) => {
+    const { nom_utilisateur, email, mot_de_passe, pays_preferee } = request.body;
+
+    pool.query(
+      'INSERT INTO users (nom_utilisateur, email, mot_de_passe, pays_preferee) VALUES ($1, $2, $3, $4)',
+      [nom_utilisateur, email, mot_de_passe, pays_preferee],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        response.status(200).json(results.rows);
+      }
+    );
+  };
+  
 
   module.exports = {
     getUsers
