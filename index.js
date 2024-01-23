15f7e1716943f8e2dd0287fd6f3608050fbdd16e
heymen <ay.alloune@gmail.com>
2024-01-23 14:04:23 +0100

    add root redirection

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views' , "./src/views");
 
+
 //Routes
+const PORT = process.env.PORT || 4111;
+app.listen(PORT, console.log("Server has started at port " + PORT));
+
+
+// BDD
+const db = require('./src/models/queries')
+const {homeView} = require("./src/controllers/indexController");
+const {router} = require("express/lib/application");
+app.get('/users', db.getUsers);
+
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
-
 app.use('/', require('./src/routes/index'));
-const PORT = process.env.PORT || 4111;
-app.listen(PORT, console.log("Server has started at port " + PORT))
-
-const db = require('./src/models/queries')
-app.get('/users', db.getUsers)
