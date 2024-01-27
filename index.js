f6765c461d3278b179a9f398bb04fdf8ccd0ec23
Sofiane.T <sofkha123@gmail.com>
2024-01-27 19:39:45 +0100

    fix inscription

--- index.js
+++ index.js
@@ -51,12 +51,13 @@ app.post("/signIn/register", async (req, res) => {
 
 
 // SignUp POST
-app.post("/account", async (req, res) => {
+app.post("/signUp/login", async (req, res) => {
     try{
         const { email, password} = req.body;
-        const user = await db.getUser(email);
-        if (password === user.mot_de_passe_hashed) {
-            res.render('account', {user});
+        const data = await db.getUser(email);
+        if (password === data.mot_de_passe_hashed) {
+            req.session.userEmail = email;
+            res.render('account', {data});
         }
         else{
             res.redirect('/signUp');
@@ -64,19 +65,37 @@ app.post("/account", async (req, res) => {
     }
     catch(error){
         console.error(error);
-        res.status(500).send('Erreur lors de la récupération des données utilisateur.');
+        res.status(500).send('Erreur lors de la connexion email ou mot de passe incorrect.');
     }
 })
 
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error ending session');
+      } else {
+        res.redirect('/home'); // Redirect to home page or login page after logging out
+      }
+    });
+  });
 function test(){
 
 app.get('/account', async (req, res) => {
     try {
         const user = await db.getUser(req.session.userEmail);
-        res.render('account', {user});
+        if(user.nom_utilisateur){
+            res.render('account', {user});
+
+        }
+     
     } catch (error) {
         console.error(error);
-        res.status(500).send('Erreur lors de la récupération des données utilisateur.');
+/*         res.status(500).send('Erreur lors de la récupération des données utilisateur. Veuillez vous reconnecter');
+ */        res.redirect('/home');
     }
 });
 }
@@ -84,10 +103,15 @@ app.get('/account', async (req, res) => {
 app.get('/profile', async (req, res) => {
     try {
         const user = await db.getUser(req.session.userEmail);
-        res.render('profile', {user});
+        if(user.nom_utilisateur){
+            res.render('profile', {user});
+
+        }
     } catch (error) {
         console.error(error);
-        res.status(500).send('Erreur lors de la récupération des données utilisateur.');
+/*         res.status(500).send('Erreur lors de la récupération des données utilisateur. Veuillez vous reconnecter');
+ */        res.redirect('/home');
+
     }
 });
 
@@ -95,10 +119,14 @@ app.get('/settings', async (req, res) => {
     try {
         const user = await db.getUser(req.session.userEmail);
         const userEmail = req.session.userEmail;
-        res.render('settings', {user, userEmail});
+        if(user.nom_utilisateur){
+            res.render('settings', {user, userEmail});
+
+        }
     } catch (error) {
         console.error(error);
-        res.status(500).send('Erreur lors de la récupération des données utilisateur.');
+/*         res.status(500).send('Erreur lors de la récupération des données utilisateur. Veuillez vous reconnecter');
+ */        res.redirect('/home');
     }
 });
 
--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -2,23 +2,49 @@ const accountModel = require('../models/queries');
 const db = require("../models/queries");
 
 const accountView = (req, res) => {
-    const data = db.getUser(req.session.userEmail);
-    res.render("account", {data});
+
+    if (req.session && req.session.userEmail) {
+        const data = db.getUser(req.session.userEmail);
+        res.render("account", { data });
+    }
+    else {
+        res.render("home");
+    }
 };
 
-const settingsView = (req, res) =>{
-    const user = db.getUser(req.session.userEmail);
-    const userEmail = req.session.userEmail;
-    res.render("settings", {user, userEmail});
+const settingsView = (req, res) => {
+
+    if (req.session && req.session.userEmail) {
+        const user = db.getUser(req.session.userEmail);
+        const userEmail = req.session.userEmail;
+        res.render("settings", { user, userEmail });
+    }
+    else {
+        res.render("home");
+    }
 }
 
-const profileView = (req, res) =>{
-    const user = db.getUser(req.session.userEmail);
-    res.render("profile", {user});
+const profileView = (req, res) => {
+    if (user && user.nom_utilisateur) {
+        db.getUser(req.session.userEmail)
+          .then((user) => {
+            res.render('profile', { user });
+          })
+          .catch((error) => {
+            console.error(error);
+            res.render('error', { message: 'Error fetching user profile' });
+          });
+      } else {
+        res.render('home');
+      }
+}
+const Logout = (req, res) => {
+    res.render("logout");
 }
 
-module.exports =  {
+module.exports = {
     accountView,
     settingsView,
-    profileView
+    profileView,
+    Logout
 };
