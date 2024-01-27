781881a957f783d1b99a53c0ee9f3825c7e77a0e
heymen <ay.alloune@gmail.com>
2024-01-27 17:21:36 +0100

    ajout backend connexion

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Initialisation
 const express = require('express');
 const session = require('express-session');
 const app = express();
 const bcrypt = require("bcryptjs");
+const url = require('node:url');
 
+// Configuration
 app.use(express.urlencoded({extended: 'false'}))
 app.use(express.json())
 app.use(session({
@@ -13,6 +16,7 @@ app.use(session({
 
 app.set('view engine', 'ejs');
 app.set('views' , "./src/views");
+
 
 //Routes
 const PORT = process.env.PORT || 4111;
@@ -25,17 +29,47 @@ const {homeView} = require("./src/controllers/indexController");
 const {router} = require("express/lib/application");
 app.get('/users', db.getUsers);
 
-app.post("/signIn/register", (req, res) => {
-    const { user, email, password, flag } = req.body;
-    console.log(req.body);
-    db.AddUser(req,res);
-    req.session.userEmail = email;
-    res.redirect('/account')
-})
 
+// Root redirection to Home
 app.get('/', (req, res) => {
     res.redirect('/home');
 });
+
+
+// SignIn POST
+app.post("/signIn/register", (req, res) => {
+    try{
+        const { user, email, password, flag } = req.body;
+        db.AddUser(req,res);
+        req.session.userEmail = email;
+        res.redirect('/account')
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).send('Erreur lors de l\'inscription de l\'utilisateur');
+    }
+})
+
+
+// SignUp POST
+app.post("/account", async (req, res) => {
+    try{
+        const { email, password} = req.body;
+        const user = await db.getUser(email);
+        if (password === user.mot_de_passe_hashed) {
+            res.render('account', {user});
+        }
+        else{
+            res.redirect('/signUp');
+        }
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).send('Erreur lors de la récupération des données utilisateur.');
+    }
+})
+
+function test(){
 
 app.get('/account', async (req, res) => {
     try {
@@ -46,6 +80,7 @@ app.get('/account', async (req, res) => {
         res.status(500).send('Erreur lors de la récupération des données utilisateur.');
     }
 });
+}
 
 app.get('/profile', async (req, res) => {
     try {
