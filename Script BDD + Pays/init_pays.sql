7ad54c37cc9d27768d615bc01cd377fc74cf55ee
Rachid Amin <aminrachid012@gmail.com>
2024-01-28 20:58:39 +0100

    Ajout scripts création bdd + implémentation pays

---
+++ Script BDD + Pays/init_pays.sql
@@ -0,0 +1,6 @@
+INSERT INTO Pays (nom_pays) VALUES ('france');
+INSERT INTO Pays (nom_pays) VALUES ('espagne');
+INSERT INTO Pays (nom_pays) VALUES ('japon');
+INSERT INTO Pays (nom_pays) VALUES ('angleterre');
+INSERT INTO Pays (nom_pays) VALUES ('italie');
+INSERT INTO Pays (nom_pays) VALUES ('autre');
